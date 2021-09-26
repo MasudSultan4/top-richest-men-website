45e134f0addda00e75eebcce9bd3bad3ec683937
MasudSultan4 <mdsarwarjahanmasudsultan1971@gmail.com>
2021-09-26 12:53:27 +0600

    new style add

--- src/component/Worth/Worth.js
+++ src/component/Worth/Worth.js
@@ -1,6 +1,7 @@
-import { faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import "./Worth.css";
 
 const Worth = (props) => {
     const { worth } = props || {}
@@ -16,11 +17,20 @@ const Worth = (props) => {
             <h4>
                 <FontAwesomeIcon icon={faUsers} /> Risest-Man: {worth.length}
             </h4>
-            <h4>Total-Worth: {total}</h4>
+            <h4>Total-Worth: ${total}</h4>
             <p>
-                {
-                    worth.map(nam => <li style={{ "list-style": "none" }} > <FontAwesomeIcon icon={faUser}/> Name: {nam.name}</li>)
-                }</p>
+            {
+                    worth.map(man => {
+                        return (
+                            <div className="d-flex align-items-center selectedActors mb-3 text-white">
+
+                                <img className="cart-img" src={man.img} alt="" />
+                                <h5>{man.name}</h5>
+                            </div>)
+                    })
+                }
+
+            </p>
 
 
 
